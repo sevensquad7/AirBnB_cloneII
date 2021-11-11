a96322885f6f18ad6905dd0e5ddf7517cba9d5db
mrgiulls <giuliano.mesias@hotmail.com>
2021-11-10 19:48:39 -0500

    exits console

--- console.py
+++ console.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+import cmd
+import models
+import datetime
+
+  class comandosAirbnb (cmd.Cmd):
+        prompt = '(HNBN)'
+        
+        def do_EOF(self, arg): 
+            """salir de la consola"""
+            return True
+        
