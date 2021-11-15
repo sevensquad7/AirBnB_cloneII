ebe1f838c85465b701061830e6947e764e8fd5f9
zye7ert <edgarxot@gmail.com>
2021-11-14 23:03:19 -0500

    add class in the console.py

--- console.py
+++ console.py
@@ -13,20 +13,26 @@ from models.place import Place
 from models.review import Review
 import models
 
-class ComandAirbnb(cmd.Cmd):
+classGpo = {"Amenity": Amenity,
+			"BaseModel": BaseModel,
+			"City": City,
+			"Place": Place,
+			"Review": Review,
+			"State": State,
+			"User": User}
+
+class HBNBCommand(cmd.Cmd):
 	""""Class Airbnb (Hbnb)"""
 	prompt = '(Hbnb)'
 	file = None
 
 	def do_quit(self, line):
-		"""Exit the Program - command quit"""
-		print('Thank you for using Airbnb console')
+		"""Quit the Program - command quit"""
 		return True
 	def do_EOF(self, line):
 		"""Exit the Program - command End of File"""
-		print('Thank you for using Airbnb console')
 		return True
 	
 
 if __name__ == '__main__' :
-	ComandAirbnb().cmdloop()
+	HBNBCommand().cmdloop()
