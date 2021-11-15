096be18a3b33fb6d90ba07da9e95f45f2317bf29
mrgiulls <giuliano.mesias@hotmail.com>
2021-11-15 15:24:18 -0500

    changes class basemodel and add kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -2,14 +2,22 @@
 """Class Base_Model"""
 from datetime import datetime, time
 import uuid
-
+time = "%Y-%m-%dT%H:%M:%S.%f"
 
 class BaseModel:
     """Date BaseModel"""
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        """Iniciando la base model"""
+        if kwargs:
+            self.__dict__ = kwargs
+            if "created_at" in kwargs:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            if "updated_at" in kwargs:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """clase de representacion del basemodel"""
@@ -21,7 +29,6 @@ class BaseModel:
 
     def to_dict(self):
         """return all content of the diccionary to keys and values"""
-        time = "%Y-%m-%dT%H:%S.%f"
         new_dict = self-__dict__.copy()
         new_dict["created_at"] = new_dict["created_at"].strftime(time)
         new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
